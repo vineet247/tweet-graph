6ca51c386af3e6276f011bdd4cc56c27a1d921d5
Vineet Viswakumar <vineetmilan@gmail.com>
2025-01-08 16:46:19 +0530

    add input arguments

--- process_tweets.py
+++ process_tweets.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 
 tweet_graph = {}
 
@@ -97,16 +98,23 @@ def delete_from_graph(tweet):
     
                     
 
-def main():
-    file_path = str(input("Enter file path: "))
-    parse_tweets(file_path)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = 'Process some tweets')
+    parser.add_argument('--filepath',type = str, required = False, help = "Path of inputs tweet file")
+    parser.add_argument('--add_tweet', type = str, required = False, help = "Tweet to be added to graph")
+    parser.add_argument('--delete_tweet', type = str, required = False, help = "Tweet to be removed from graph")
+    args = parser.parse_args()
+
+    if not args.filepath:
+        print("No filepath provided")
+        quit()
+    parse_tweets(args.filepath)
     avg_degree = calculate_avg_degree()
     print(avg_degree)
 
-    # tweet = "{\"created_at\":\"Mon Mar 28 23:23:12 +0000 2016\",\"id\":714593712366620672,\"id_str\":\"714593712366620672\",\"text\":\"@gbvigo miga isso nao existe vc \\u00e9 taurina rssa\",\"source\":\"\\u003ca href=\\\"http:\\/\\/twitter.com\\/download\\/iphone\\\" rel=\\\"nofollow\\\"\\u003eTwitter for iPhone\\u003c\\/a\\u003e\",\"truncated\":false,\"in_reply_to_status_id\":714593258802905089,\"in_reply_to_status_id_str\":\"714593258802905089\",\"in_reply_to_user_id\":154304713,\"in_reply_to_user_id_str\":\"154304713\",\"in_reply_to_screen_name\":\"gbvigo\",\"user\":{\"id\":1486482758,\"id_str\":\"1486482758\",\"name\":\"raphaella\",\"screen_name\":\"voudaropapo\",\"location\":\"Rio de Janeiro, Brasil\",\"url\":null,\"description\":\"arquitetei universos s\\u00f3 com a minha paranoia\",\"protected\":false,\"verified\":false,\"followers_count\":394,\"friends_count\":287,\"listed_count\":0,\"favourites_count\":9598,\"statuses_count\":29616,\"created_at\":\"Thu Jun 06 01:15:57 +0000 2013\",\"utc_offset\":null,\"time_zone\":null,\"geo_enabled\":true,\"lang\":\"pt\",\"contributors_enabled\":false,\"is_translator\":false,\"profile_background_color\":\"000000\",\"profile_background_image_url\":\"http:\\/\\/abs.twimg.com\\/images\\/themes\\/theme18\\/bg.gif\",\"profile_background_image_url_https\":\"https:\\/\\/abs.twimg.com\\/images\\/themes\\/theme18\\/bg.gif\",\"profile_background_tile\":false,\"profile_link_color\":\"F5ABB5\",\"profile_sidebar_border_color\":\"000000\",\"profile_sidebar_fill_color\":\"000000\",\"profile_text_color\":\"000000\",\"profile_use_background_image\":false,\"profile_image_url\":\"http:\\/\\/pbs.twimg.com\\/profile_images\\/712411700436320256\\/V0FuQVuG_normal.jpg\",\"profile_image_url_https\":\"https:\\/\\/pbs.twimg.com\\/profile_images\\/712411700436320256\\/V0FuQVuG_normal.jpg\",\"profile_banner_url\":\"https:\\/\\/pbs.twimg.com\\/profile_banners\\/1486482758\\/1459036011\",\"default_profile\":false,\"default_profile_image\":false,\"following\":null,\"follow_request_sent\":null,\"notifications\":null},\"geo\":null,\"coordinates\":null,\"place\":{\"id\":\"97bcdfca1a2dca59\",\"url\":\"https:\\/\\/api.twitter.com\\/1.1\\/geo\\/id\\/97bcdfca1a2dca59.json\",\"place_type\":\"city\",\"name\":\"Rio de Janeiro\",\"full_name\":\"Rio de Janeiro, Brasil\",\"country_code\":\"BR\",\"country\":\"Brasil\",\"bounding_box\":{\"type\":\"Polygon\",\"coordinates\":[[[-43.795449,-23.083020],[-43.795449,-22.739823],[-43.087707,-22.739823],[-43.087707,-23.083020]]]},\"attributes\":{}},\"contributors\":null,\"is_quote_status\":false,\"retweet_count\":0,\"favorite_count\":0,\"entities\":{\"hashtags\":[],\"urls\":[],\"user_mentions\":[{\"screen_name\":\"gbvigo\",\"name\":\"gab.\",\"id\":154304713,\"id_str\":\"154304713\",\"indices\":[0,7]}],\"symbols\":[]},\"favorited\":false,\"retweeted\":false,\"filter_level\":\"low\",\"lang\":\"pt\",\"timestamp_ms\":\"1459207392194\"}\r\n"
-    # new_avg = delete_from_graph(tweet)
-    # print("new avg: ", new_avg)
-
-if __name__ == '__main__':
-    main()
+    #if user wants to add a tweet to the graph
+    if args.add_tweet:
+        insert_to_graph(str(args.add_tweet))
+        new_avg_degree = calculate_avg_degree()
+        print("new average degree: ", new_avg_degree)
         
